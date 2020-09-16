504347fc88ce89266520196b8df0f7ae95c6bd3a
Joe Terry <joe@joeterry.org>
2020-09-15 17:30:25 -0700

    initial commit on what I hope will be the ongoing best practices server for the Apollo Standard Tutorial

---
+++ README.md
@@ -0,0 +1,20 @@
+barpass-graphql
+
+Houses data layer for flutter development of barpass currently hosted on Heroku.
+
+https://barpass-server.herokuapp.com/
+
+This is an MVP of an Apollo Powered (https://www.apollographql.com/) implementation of GraphQL an open source specification for building powerful retrival and storage models of any data sourced from anywhere.
+
+This GraphQL Server is hosted on Heroku and backed by the Postgresql database (https://www.postgresql.org/), the most advanced open source relational database.
+
+This same server with NO CHANGES can be run locally on a desktop computer in which case it is backed by the SQLite file-based database which powers every mobile phone from Apple's IOS and Android's Android.
+
+Managing data in modern applications is challenging. Most applications require:
+
+Distinct front-end clients for multiple platforms (web, iOS, etc.), each with different data requirements
+A backend that serves data to clients from multiple sources (Postgres, Redis, etc.)
+Complex state and cache management for both the frontend and the backend
+By adopting GraphQL and Apollo, you can reduce these challenges considerably. GraphQL's declarative model helps you create a consistent, predictable API you can use across all of your clients. As you add, remove, and migrate back-end data stores, that API doesn't change from the client's perspective.
+
+Even with many other advantages, GraphQL's single greatest benefit is the developer experience it provides. It's straightforward to add new types and fields to your API, and similarly straightforward for your clients to begin using those fields. This helps you design, develop, and deploy features quickly.
---
+++ src/utils.js
@@ -0,0 +1,71 @@
+const {Sequelize} = require('sequelize');
+const {sqlite} = require('sqlite3'); // Needed to fix sequelize issues with WebPack
+
+module.exports.paginateResults = ({
+  after: cursor,
+  pageSize = 20,
+  results,
+  // can pass in a function to calculate an item's cursor
+  getCursor = () => null,
+}) => {
+  if (pageSize < 1) return [];
+
+  if (!cursor) return results.slice(0, pageSize);
+  const cursorIndex = results.findIndex(item => {
+    // if an item has a `cursor` on it, use that, otherwise try to generate one
+    let itemCursor = item.cursor ? item.cursor : getCursor(item);
+
+    // if there's still not a cursor, return false by default
+    return itemCursor ? cursor === itemCursor : false;
+  });
+
+  return cursorIndex >= 0
+    ? cursorIndex === results.length - 1 // don't let us overflow
+      ? []
+      : results.slice(
+          cursorIndex + 1,
+          Math.min(results.length, cursorIndex + 1 + pageSize),
+        )
+    : results.slice(0, pageSize);
+};
+
+module.exports.createStore = () => {
+
+  const db = (() => {
+    if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+      // the application is executed on Heroku ... use the postgres database
+      return new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true //false
+      })
+    } else {
+      // the application is executed on the local machine ... use sqlite
+      return new Sequelize({
+        dialect: 'sqlite',
+        dialectModule: sqlite, // Needed to fix sequelize issues with WebPack
+        storage: './store.sqlite'
+      });
+    }
+  })();
+
+
+  const users = db.define('user', {
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+    email: Sequelize.STRING,
+    profileImage: Sequelize.STRING,
+    token: Sequelize.STRING,
+  });
+
+  const trips = db.define('trip', {
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+    launchId: Sequelize.INTEGER,
+    userId: Sequelize.INTEGER,
+  });
+
+  return { db, users, trips };
+};
